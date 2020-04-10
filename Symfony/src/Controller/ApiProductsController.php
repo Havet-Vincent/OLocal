8a755d6c460dafef3034c737da262dea87847195
Nicolas-Chopin <chopin.nico@gmail.com>
2020-04-10 10:28:10 +0200

    api's controllers creation

---
+++ Symfony/src/Controller/ApiProductsController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ApiProductsController extends AbstractController
+{
+    /**
+     * @Route("/api/products", name="api_products")
+     */
+    public function index()
+    {
+        return $this->render('api_products/index.html.twig', [
+            'controller_name' => 'ApiProductsController',
+        ]);
+    }
+}
