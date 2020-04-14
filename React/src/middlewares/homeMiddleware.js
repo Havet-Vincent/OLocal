c66846736b9d7ea9f6e9fccbfdaa4fd50e386ad8
Olivier-Go <o.gonnet@gmail.com>
2020-04-14 17:01:26 +0200

    fix POST Request for search shopkeepers

--- React/src/middlewares/homeMiddleware.js
+++ React/src/middlewares/homeMiddleware.js
@@ -7,7 +7,7 @@ const homeMiddleware = (store) => (next) => (action) => {
     case GET_REGIONS_DATA:
       axios.get('http://nicolas-chopin.vpnuser.lan:8001/api/regions')
         .then((response) => {
-          console.log('success regions : ', response.data);
+          // console.log('success regions : ', response.data);
           store.dispatch(saveRegionsData(response.data));
         })
         .catch((error) => {
@@ -20,7 +20,7 @@ const homeMiddleware = (store) => (next) => (action) => {
     case GET_CATEGORIES_DATA:
       axios.get('http://nicolas-chopin.vpnuser.lan:8001/api/categories')
         .then((response) => {
-          console.log('success categories : ', response.data);
+          // console.log('success categories : ', response.data);
           store.dispatch(saveCategoriesData(response.data));
         })
         .catch((error) => {
@@ -36,18 +36,15 @@ const homeMiddleware = (store) => (next) => (action) => {
         "category": ${store.getState().home.categoryField}
       }`);
       axios({
-        method: 'get',
-        url: 'http://nicolas-chopin.vpnuser.lan:8001/shopkeepers',
-        headers: {
-          'Content-Type': 'application/json'
-        },
+        method: 'post',
+        url: 'http://nicolas-chopin.vpnuser.lan:8001/api/shopkeepers',
         data: {
           region: store.getState().home.regionField,
           category: store.getState().home.categoryField
         }
       })
         .then((response) => {
-          console.log('success categories : ', response.data);
+          console.log('success: ', response.data);
           // store.dispatch(saveUser(response.data.logged, response.data.info));
         })
         .catch((error) => {
