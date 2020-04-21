e2061610695f40f2f273761a066f3149cd6a2cc7
HAVET <vhavet@gmail.com>
2020-04-21 14:10:53 +0200

    fix shopkeeperProfilPages

--- React/src/components/Pages/Profil/ShopkeeperProfil/ShopkeeperProfilPage/index.js
+++ React/src/components/Pages/Profil/ShopkeeperProfil/ShopkeeperProfilPage/index.js
@@ -4,7 +4,7 @@ import { Link as RouterLink } from 'react-router-dom';
 // == Import components
 import { Paper, IconButton, Container } from '@material-ui/core';
 import MaterialTable from 'material-table';
-import MUIDataTable from "mui-datatables";
+
 
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 
@@ -30,54 +30,65 @@ const Alert = (props) => {
 // == Import API server config
 const server = require('src/api.config.json');
 
+
+const onDelete = (data) => {
+  // console.log('delete ', data);
+}
+const onAdd = (data) => {
+  // console.log('add ', data);
+}
+
+const onUpdate = (newData) => {
+  // console.log('update ', newData);
+}
+
+  const catalog = shopkeeper.catalogs.map((catalog) => (
+    { 
+      catalogId: catalog.id,
+      category: catalog.product.category.name,
+      product: catalog.product.name,
+      supplier: catalog.localSupplier.name,
+      city: catalog.localSupplier.city,
+      postalCode: catalog.localSupplier.postalCode,
+    }
+  ))
+
+// console.log('catalog', catalog);
+
 // == Composant
 const ShopkeeperProfilPage = () => {
   const classes = shopkeeperProfilPageStyles()
 
 
+
   const [state, setState] = useState({
-    columns: [
-      {
-        title: 'Catégorie', field: 'category', cellStyle: {
-          backgroundColor: '#039be5',
-          color: '#FFF'
-        },
+    columns: [ 
+      { title: 'Catégorie', 
+        field: 'category',
+
+      
+        headerStyle: { backgroundColor: '#039be5' },
+        cellStyle: { backgroundColor: '#03923d', color: '#FFF'},
+      }, 
+      { title: 'Article', field: 'product', headerStyle: {
+        backgroundColor: '#039be5',
+      }  },
+      { title: 'Producteur', 
+        field: 'supplier', 
+
         headerStyle: {
-          backgroundColor: '#eee',
-        }
-      },
-      { title: 'Article', field: 'product' },
-      { title: 'Producteur', field: 'supplier' },
-      {
-        title: 'Localité',
-        field: 'city',
-      },
-      { title: 'Code postal', field: 'postalCode', lookup: { 75: 'Paris', 74: 'Bloix' }, },
+        backgroundColor: '#039be5',
+      }  },
+      { title: 'Localisation', field: 'city', headerStyle: {
+        backgroundColor: '#039be5',
+      }  },
+      { title: 'Code Postal', field: 'postalCode', headerStyle: {
+        backgroundColor: '#039be5',
+      }  },
     ],
-    data: [
-      {
-        category: 'Fruits',
-        product: 'adipisci',
-        supplier: 'Leblanc S.A.S.',
-        city: 'Paris',
-        postalCode: 75,
-
-      },
-      {
-        category: 'savon noir',
-        product: 'Entretien et Nettoyage',
-        supplier: 'Perrier',
-        city: 'Bloix',
-        postalCode: 74,
-      },
-    ],
+    data: catalog,
+   
   });
-
-  const columns = ["Catégorie", "Article", "Producteur", "Localisation"];
-  const dataBis = [
-    ["Fruits", "adipisci", "Leblanc S.A.S.", "Paris"],
-    ["savon noir", "Entretien et Nettoyage", "Perrier", "Bloix"],
-  ];
 
   return (
     <Container className={classes.shopkeeperProfilContent} component="main" maxWidth="lg">
@@ -86,68 +97,59 @@ const ShopkeeperProfilPage = () => {
           <ArrowBackIcon fontSize="large" color="action" />
         </IconButton>
         <NavbarShopkeeperProfil />
-
-        <MaterialTable
-          title="Liste des articles par catégories et par producteurs"
-          columns={state.columns}
-          data={state.data}
-          editable={{
-            onRowAdd: (newData) =>
-              new Promise((resolve) => {
-                setTimeout(() => {
-                  resolve();
-                  setState((prevState) => {
-                    const data = [...prevState.data];
-                    data.push(newData);
-                    return { ...prevState, data };
-                  });
-                }, 600);
-              }),
-            onRowUpdate: (newData, oldData) =>
-              new Promise((resolve) => {
-                setTimeout(() => {
-                  resolve();
-                  if (oldData) {
-                    setState((prevState) => {
-                      const data = [...prevState.data];
-                      data[data.indexOf(oldData)] = newData;
-                      return { ...prevState, data };
-                    });
-                  }
-                }, 600);
-              }),
-            onRowDelete: (oldData) =>
-              new Promise((resolve) => {
-                setTimeout(() => {
-                  resolve();
-                  setState((prevState) => {
-                    const data = [...prevState.data];
-                    data.splice(data.indexOf(oldData), 1);
-                    return { ...prevState, data };
-                  });
-                }, 600);
-              }),
-          }}
-          options={{
-            filtering: true
-          }}
-          actions={[
-            {
-              icon: 'save',
-              tooltip: 'Save User',
-              onClick: (event, rowData) => alert("You saved " + rowData.name)
-            }
-          ]}
-        />
-
-        <MUIDataTable
-          title={"Article par gatégorie et producteur"}
-          data={dataBis}
-          columns={columns}
-          options={options}
-        />
-  );
       </Paper>
+      <Paper>
+      <MaterialTable
+      title="Article par catégorie et Producteur"
+      columns={state.columns}
+      data={state.data}
+      editable={{
+        onRowAdd: (newData) =>
+          new Promise((resolve) => {
+            setTimeout(() => {
+              onAdd(newData);
+              resolve();
+              setState((prevState) => {
+                const data = [...prevState.data];
+                data.push(newData);
+                return { ...prevState, data };
+              });
+            }, 600);
+          }),
+          onRowUpdate: (newData, oldData) =>
+          new Promise((resolve) => {
+            setTimeout(() => {
+              onUpdate(newData);
+              resolve();
+              if (oldData) {
+                setState((prevState) => {
+                  const data = [...prevState.data];
+                  data[data.indexOf(oldData)] = newData;
+                  return { ...prevState, data };
+                });
+              }
+            }, 600);
+          }),
+        onRowDelete: (oldData) =>
+          new Promise((resolve) => {
+            setTimeout(() => {
+              onDelete(oldData);
+              resolve();
+              setState((prevState) => {
+                const data = [...prevState.data];
+                data.splice(data.indexOf(oldData), 1);
+                return { ...prevState, data };
+              });
+            }, 600);
+          }),
+      }}
+      options={{
+        exportButton: true,
+        filtering: true,  
+      }}
+    />
+      </Paper>
+
     </Container>
   );
 };
@@ -155,3 +157,6 @@ const ShopkeeperProfilPage = () => {
 
 // == Export
 export default ShopkeeperProfilPage;
+
+
+
