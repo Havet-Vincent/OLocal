c0b6c21229c4dd61be1411ba99ac7b7a6dd7cdcd
Olivier-Go <o.gonnet@gmail.com>
2020-04-29 10:50:17 +0200

    modify contact backgroung & css

--- React/src/components/Pages/Contact/contactStyles.js
+++ React/src/components/Pages/Contact/contactStyles.js
@@ -1,5 +1,5 @@
 import { makeStyles } from '@material-ui/core/styles';
-import BackgroundImg from 'src/assets/img/landscape_background.png';
+import BackgroundImg from 'src/assets/img/background_contact.png';
 
 const contactStyles = makeStyles((theme) => ({
 
@@ -9,26 +9,64 @@ const contactStyles = makeStyles((theme) => ({
     backgroundSize: 'cover',
     backgroundPosition: 'center',
     flexGrow: 1,
-    justifyContent: 'center',
     height: '100%',
     width: '100%',
+    display: 'flex',
+    flexFlow: 'column nowrap',
+    justifyContent: 'center',
   },
   container: {
     display: 'flex',
     flexFlow: 'column nowrap',
+    justifyContent: 'flex-end',
     alignItems: 'center',
-    marginTop: '400px',
-    marginBottom: '100px',
-    padding: theme.spacing(1, 2),
+    marginTop: 280,
+    marginBottom: '8rem',
+    width: 420,
     paddingBottom: theme.spacing(5),
-    [theme.breakpoints.down('xs')]: {
-      padding: theme.spacing(1),
+    paddingLeft: theme.spacing(6),
+    [theme.breakpoints.down('md')]: {
+      marginTop: 250,
+      width: '70%',
+      height: 300,
+      paddingLeft: theme.spacing(9.5),
     },
-},
+    [theme.breakpoints.down('sm')]: {
+      width: '70%',
+      marginTop: 200,
+      padding: theme.spacing(1),
+      paddingLeft: theme.spacing(3.6),
+    },
+  },
   paper: {
     background: 'none',
     flexGrow: 1,
     alignItems: 'center',
+  },
+  title: {
+    fontWeight: 700,
+    padding: theme.spacing(0, 4, 1, 4),
+    width: '100%',
+    margin: '0 auto',
+    [theme.breakpoints.down('lg')]: {
+      fontSize: '1.8em',
+    },
+    [theme.breakpoints.down('md')]: {
+      width: '50%',
+      margin: '0 auto',
+    },
+    [theme.breakpoints.down('sm')]: {
+      width: '50%',
+      fontSize: '1.55em',
+      margin: '0 auto',
+      padding: theme.spacing(0, 2, 0, 4),
+    },
+    [theme.breakpoints.down('xs')]: {
+      width: '80%',
+      fontSize: '1.5em',
+      margin: '0 auto',
+      padding: theme.spacing(0, 0, 0, 1.2),
+    },
   },
   contact: {
     display: 'flex',
@@ -37,10 +75,9 @@ const contactStyles = makeStyles((theme) => ({
     padding: theme.spacing(1),
 
   },
-  contactIcons : {
+  contactIcons: {
     marginRight: '5px',
-  }
-  
+  },
 }));
 
 export default contactStyles;
